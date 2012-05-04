32d0b3cde437d3ceec94e5e71b37985ba2771ff9
Alexey Kosarev <alex@homyaki.info>
2012-05-04 21:10:56 +0400

    Move interface handlers to DB st3 database

--- db/add_form.sql
+++ db/add_form.sql
@@ -1,4 +1,4 @@
-use homyaki_web;
+USE `homyaki_web`;
 INSERT INTO `form_handlers` (handler, name, description, interface_name)
 	VALUES	
 		('Homyaki::Interface::Default'     , 'main'  , 'Default form handler'    , 'main')
