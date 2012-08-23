86a55fafaa1f1c24d15e544a1475ce3fe0ee55cd
Alexey Kosarev <alex@homyaki.info>
2012-08-23 20:02:55 +0400

    Navigation Simple (Small text)

--- libs/Interface/Navigation/Simple.pm
+++ libs/Interface/Navigation/Simple.pm
@@ -67,7 +67,7 @@ sub add_navigation {
 
 	my $navigation_list_tag = $navigation_col->add(
 		type => &TAG_LIST,
-		&PARAM_STYLE => 'padding:0px;'
+		&PARAM_STYLE => 'padding:0px;margin:0px;'
 	);
 
 	my $group_count = 0;
@@ -120,7 +120,7 @@ sub set_parrent_group_opened{
 	my $parrent_group = shift;
 
 	if ($parrent_group->{type} eq &TAG_LIST) {
-		$parrent_group->{&PARAM_STYLE} =  'display:block;padding:5px;';
+		$parrent_group->{&PARAM_STYLE} =  'display:block;padding:5px;margin:0px;';
 	}
 	if ($parrent_group->{type} eq &TAG_LIST || $parrent_group->{type} eq &TAG_LIST_ITEM) {
 		set_parrent_group_opened($parrent_group->{parrent})
@@ -142,11 +142,11 @@ sub add_navigation_items {
 				my $sub_menue_item = $list_tag->add(
 					type           => &TAG_LIST_ITEM,
 					body_before    => $menue_item . ' <b style="font-size:16px" id="button_' . $$group_count . '" onClick="show_hide(\'' . $$group_count . '\');">+</b>',
-					&PARAM_STYLE   => 'list-style-type: none; color:#666666;font-size:10px',
+					&PARAM_STYLE   => 'list-style-type: none; color:#666666;font-size:10px;margin-top: 8px;',
 				);
 				my $sub_menue_list = $sub_menue_item->add(
 					type         => &TAG_LIST,
-					&PARAM_STYLE => 'display:none;padding:5px;',
+					&PARAM_STYLE => 'display:none;padding:5px;margin:0px;',
 					&PARAM_ID    => "group_$$group_count", 
 				);
 				$this->add_navigation_items(
@@ -167,7 +167,7 @@ sub add_navigation_items {
 				}
 				my $navigation_item_tag = $list_tag->add(
 					type         => &TAG_LIST_ITEM,
-					&PARAM_STYLE => $current_item ? 'color:#DDDDDD; list-style-type: circle;font-size:10px' : 'list-style-type: none;font-size:10px',	
+					&PARAM_STYLE => $current_item ? 'color:#DDDDDD; list-style-type: circle;font-size:10px;margin-top: 8px;' : 'list-style-type: none;font-size:10px;margin-top: 8px;',	
 				);
 				$navigation_item_tag->add(
 					type         => &TAG_A,
