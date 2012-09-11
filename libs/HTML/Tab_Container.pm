c8629dfd1a267df88f84262da1f9460cdf968db2
Alexey Kosarev <alex@homyaki.info>
2012-09-11 16:40:40 +0400

    Tabs panel widget (Fix some relase bugs)

--- libs/HTML/Tab_Container.pm
+++ libs/HTML/Tab_Container.pm
@@ -25,11 +25,16 @@ sub add_tab_container {
 
 	my $tab_panels  = $h{tab_panels};
 
-	my $tab_panel_table = $body_tag->add(
+	my $tab_panel_table = $body_tag->add_form_element(
 		name   => "tab_panel_$name",
+		type   => &INPUT_TYPE_DIV,
+	);
+	
+	$tab_panel_table = $tab_panel_table->add(
 		type   => &TAG_TABLE,
 	);
 
+	
 	if (scalar(keys %{$tab_panels}) > 0) {
 
 		if (scalar(keys %{$tab_panels}) > 1) {
@@ -47,7 +52,6 @@ sub add_tab_container {
 				push(@{$tab_element_names}, "tab_panel_${name}_body_row_$tab_panel->{name}");
 
 				my $tab_panel_button = $tab_panel_navi->add(
-					name => "tab_panel_${name}_cell_$tab_panel->{name}",
 					type => &TAG_COLUMN,
 				);
 
@@ -55,18 +59,21 @@ sub add_tab_container {
 					type           => &TAG_INPUT,
 					&PARAM_TYPE    => 'button',
 					&PARAM_ONCLICK => "tab_panel_${name}_on_click('tab_panel_${name}_body_row_$tab_panel->{name}');",
-					&PARAM_STYLE   => $params->{"tab_panel_${name}"} eq $tab_panel->{name} ? 'display:inline;' : 'display:none;',
-					body           => $tab_panel->{caption},
+					&PARAM_VALUE   => $tab_panel->{header},
 				);
 
 				my $tab_panel_body_row = $tab_panel_table->add(
-					name   => "tab_panel_${name}_body_row_$tab_panel->{name}",
-					type   => &TAG_ROW,
+					&PARAM_ID      => "tab_panel_${name}_body_row_$tab_panel->{name}",
+					&PARAM_STYLE   => $params->{"tab_panel_${name}"} eq $tab_panel->{name} ? 'display:inline;' : 'display:none;',
+					type           => &TAG_ROW,
 				);
 				my $tab_panel_body_form = $tab_panel_body_row->add(
-					name           => "tab_panel_${name}_body_col_$tab_panel->{name}",
 					type           => &TAG_COLUMN,
-					&PARAM_COLSPAN => scalar(@{$tab_panels}),
+					&PARAM_COLSPAN => scalar(keys %{$tab_panels}),
+				);
+
+				$tab_panel_body_form = $tab_panel_body_form->add(
+					type   => &TAG_TABLE,
 				);
 
 				$tab_panel->{form_body} = $tab_panel_body_form;			
@@ -80,29 +87,31 @@ sub add_tab_container {
 						$tab_elements_js
 					);
 
-					procedure tab_panel_${name}_on_click(tab_name){
+					function tab_panel_${name}_on_click(tab_name){
 						for (var i = 0; i < tab_${name}_elements.length; i++) { 
-							tabElement = document.getElementById(tab_name);
-						if (tab_name eq tab_${name}_elements[i]){
-							tabElement.style = 'display:inline';
+							tabElement = document.getElementById(tab_${name}_elements[i]);
+						if (tab_name == tab_${name}_elements[i]){
+							tabElement.style.display = 'inline';
 							} else {
-								tabElement.style = 'display:none';
+								tabElement.style.display = 'none';
 							}
 						}
 					}
 				|
 			);
 		} else {
-			my $tab_panel = [keys %{$tab_panels}]->[0];
+			my $tab_panel = $tab_panels->{[keys %{$tab_panels}]->[0]};
 
 			my $tab_panel_body_row = $tab_panel_table->add(
-				name   => "tab_panel_${name}_body_row_$tab_panel->{name}",
 				type   => &TAG_ROW,
 			);
 
 			my $tab_panel_body_form = $tab_panel_body_row->add(
-				name           => "tab_panel_${name}_body_col_$tab_panel->{name}",
-				type           => &TAG_COLUMN,
+				type   => &TAG_COLUMN,
+			);
+
+			$tab_panel_body_form = $tab_panel_body_form->add(
+				type   => &TAG_TABLE,
 			);
 
 			$tab_panel->{form_body} = $tab_panel_body_form;			
